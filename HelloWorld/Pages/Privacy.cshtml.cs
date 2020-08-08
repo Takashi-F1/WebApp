94d83b6489f468d54dcc738c517e50ccbcf82ae5
Takashi Fujimoto <ft2020sh@gmail.com>
2020-08-08 13:38:48 +0900

    WebApp:いろいろ試した

--- HelloWorld/Pages/Privacy.cshtml.cs
+++ HelloWorld/Pages/Privacy.cshtml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
 
 namespace HelloWorld.Pages
 {
@@ -19,6 +16,8 @@ namespace HelloWorld.Pages
 
         public void OnGet()
         {
+            string datetime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = datetime;
         }
     }
 }
