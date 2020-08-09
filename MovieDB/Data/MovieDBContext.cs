32a937e43e68d10cf267934f33acc967bd033936
Takashi Fujimoto <ft2020sh@gmail.com>
2020-08-09 16:08:48 +0900

    MovieDB:スキャホールディング

---
+++ MovieDB/Data/MovieDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieDB.Models;
+
+namespace MovieDB.Data
+{
+    public class MovieDBContext : DbContext
+    {
+        public MovieDBContext (DbContextOptions<MovieDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieDB.Models.Movie> Movie { get; set; }//エンティティセット（データベースのテーブルに対応）
+    }
+}
