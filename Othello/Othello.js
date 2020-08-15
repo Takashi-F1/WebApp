23ea387abbad9cbd069487edba29bd65394832ac
Takashi Fujimoto <ft2020sh@gmail.com>
2020-08-15 23:26:58 +0900

    Othello：置くことが出きるマスの判定関数作成・それぞれの石のカウントと表示

--- Othello/Othello.js
+++ Othello/Othello.js
@@ -1,108 +1,119 @@
-/*set stone*/
-function setstone(dum) {
-  var playernum = document.getElementById("player").innerText;
-  var cellid = dum.id;
-  var cell = document.getElementById(cellid).innerText;
-  var x_id = cellid.slice(-1); //x coordinate
-  var y_id = cellid.slice(0, 1); // y coordinate
-  var check_dummy;
-  /*check setting possibility */
+var direction = [-11, -10, -9, -1, 1, 9, 10, 11];
+var SymbolList = ["〇", "●"];
 
-  if (!cell) { //選択したセルが空か確認
-    var neighbor = [];
-    var direction = [-11, -10, -9, -1, 1, 9, 10, 11];
-    var mystone_check;
-
-    check1:
-    for (var i = 0; i < 8; i++) {
-      /*周囲８方向に自分以外の色がないと置けないのでチェック。*/
-
-      check_dummy = String(parseInt(cellid) + direction[i]);
-      if (parseInt(check_dummy) > 10 && parseInt(check_dummy.slice(-1)) > 0 &&
-        parseInt(check_dummy.slice(-1)) < 9 && parseInt(check_dummy.slice(0, 1)) > 0 &&
-        parseInt(check_dummy.slice(0, 1)) < 9) { //周囲８方向の盤面外はチェックしない
-
-        alert("neighbor" + (i + 1) + "の確認");
-
-        neighbor[i] = document.getElementById(String(parseInt(cellid) + direction[i])).innerText;
-        if (neighbor[i] !== "") {
-          //neighbor[i]が空じゃないときにそのセルの色を確認
-          if (playernum == 1 && neighbor[i] == "✖") { //周囲のある方向に相手の石があった場合
-            alert("この方向に相手の石ある")
-
-            for (var j = 2; j < 8; j++) { //その先に自分の石があるか確認
-              check_dummy = String(parseInt(cellid) + direction[i] * j);
-              alert(check_dummy);
-              if (parseInt(check_dummy) > 10 && parseInt(check_dummy.slice(-1)) > 0 &&
-                parseInt(check_dummy.slice(-1)) < 9 && parseInt(check_dummy.slice(0, 1)) > 0 &&
-                parseInt(check_dummy.slice(0, 1)) < 9) { //盤面外はチェックしない
-
-                mystone_check = document.getElementById(String(parseInt(cellid) + direction[i] * j)).innerText;
-                // alert(mystone_check);
-                if (mystone_check !== "") { //mystone_checkが空じゃない場合
-                  if (mystone_check == "〇") {
-                    alert("そこに置けます");
-                    document.getElementById(cellid).innerText = "〇";
-                    document.getElementById("player").innerText = 2;
-                    break check1; //置けるなら周囲の確認終了
-                  }
-                } else {//mystone_checkが空の場合、その方向はチェックする必要ない
-                  alert("この方向には自分の石がありません")
-                  break;
-                }
-              } else {//先が盤面外ならそれ以上先は調べる必要ない
-                alert("この方向には自分の石がありません");
-                break;
-              }
-              if (j == 7) {
-                alert("この方向には自分の石がありません");
-              }
-            }
-          } else if (playernum == 2 && neighbor[i] == "〇") {
-            alert("この方向に相手の石ある")
-
-            for (var j = 2; j < 8; j++) { //その先に自分の石があるか確認
-              check_dummy = String(parseInt(cellid) + direction[i] * j);
-              alert(check_dummy);
-              if (parseInt(check_dummy) > 10 && parseInt(check_dummy.slice(-1)) > 0 &&
-                parseInt(check_dummy.slice(-1)) < 9 && parseInt(check_dummy.slice(0, 1)) > 0 &&
-                parseInt(check_dummy.slice(0, 1)) < 9) { //盤面外はチェックしない
-
-                mystone_check = document.getElementById(String(parseInt(cellid) + direction[i] * j)).innerText;
-                // alert(mystone_check);
-                if (mystone_check !== "") { //mystone_checkが空じゃない場合
-                  if (mystone_check == "✖") {
-                    alert("そこに置けます");
-                    document.getElementById(cellid).innerText = "✖";
-                    document.getElementById("player").innerText = 1;
-                    break check1; //置けるなら周囲の確認終了
-                  }
-                } else {//mystone_checkが空の場合、その方向はチェックする必要ない
-                  alert("この方向には自分の石がありません");
-                  break;
-                }
-              } else {//先が盤面外ならそれ以上先は調べる必要ない
-                alert("この方向には自分の石がありません");
-                break;
-              }
-              if (j == 7) {
-                alert("この方向には自分の石がありません");
-              }
-            }
-          }
-        } //neighbor[i]が空なら次の方向をチェックi++
-      } //盤面外なら無視
-      if (i == 7) {
-        alert("このマスには置けません。");
+/*count*/
+function count_stone() {
+  var count_P1 = 0;
+  var count_P2 = 0;
+  var count_empty = 0;
+  for (x = 1; x < 9; x++) {
+    for (y = 1; y < 9; y++) {
+      var cell_id = String(x) + String(y);
+      var cell = document.getElementById(cell_id).innerText;
+      if (cell == "〇") {
+        count_P1++;
+      } else if (cell == "●") {
+        count_P2++;
       }
     }
+  }
+  count_empty = 64 - count_P1 - count_P2;
+  document.getElementById("num_p1").innerText = count_P1;
+  document.getElementById("num_p2").innerText = count_P2;
+  document.getElementById("empty").innerText = count_empty;
 
+}
+
+/*check cell*/
+function check_cell(selected) {
+  //選択されたセルに置くことができない場合は[-1,-1]を、置ける場合はdirection[i]のインデックスiと、自分のマスまでのセル数jを配列[i,j]で返す。
+  var playernum = document.getElementById("player").innerText;
+  var cellid = selected.id;
+  var cell = document.getElementById(cellid).innerText;
+  var CheckOppCell;
+  var CheckMyCell;
+
+  /*check setting possibility */
+  if (!cell) { //選択したセルが空か確認
+    var neighbor = [];
+
+    CheckLoop:
+    for (var i = 0; i < 8; i++) {      /*周囲８方向に相手の石があるかチェック*/
+      CheckOppCell = String(parseInt(cellid) + direction[i]);
+
+      if (parseInt(CheckOppCell) < 11 || parseInt(CheckOppCell.slice(-1)) < 1 || parseInt(CheckOppCell.slice(-1)) > 8 ||
+        parseInt(CheckOppCell.slice(0, 1)) < 1 || parseInt(CheckOppCell.slice(0, 1)) > 8) { //盤面外の場合はbreakして次のdirectionを確認
+        if (i == 7) {
+          //alert("このマスには置けません。");
+          return [-1, -1];
+        }
+        continue;
+      }
+
+      //alert("neighbor" + (i + 1) + "の確認");
+      neighbor[i] = document.getElementById(CheckOppCell).innerText;
+      if (neighbor[i] == SymbolList[playernum % 2]) { //周囲8方向に相手の石があった場合
+        for (var j = 2; j < 8; j++) {
+          /*その方向の先に自分の石があるか確認*/
+          CheckMyCell = String(parseInt(cellid) + direction[i] * j);
+
+          if (parseInt(CheckMyCell) < 11 || parseInt(CheckMyCell.slice(-1)) < 1 || parseInt(CheckMyCell.slice(-1)) > 8 ||
+            parseInt(CheckMyCell.slice(0, 1)) < 1 || parseInt(CheckMyCell.slice(0, 1)) > 8) {  //盤面外はbreak
+            break;
+          }
+          CheckMyCell_value = document.getElementById(CheckMyCell).innerText;
+          if (CheckMyCell_value !== "") { //CheckMyCell_valueが空じゃない場合
+            if (CheckMyCell_value == SymbolList[playernum - 1]) {//自分の石が見つかった場合
+              // for (var k = 0; k <= j; k++) {
+              //   /*選択されたセルに石を置く＆挟んだ相手の石の色を変える*/
+              //   document.getElementById(String(parseInt(cellid) + direction[i] * k)).innerText = SymbolList[playernum - 1];
+              // }
+              //document.getElementById("player").innerText = playernum % 2 + 1;
+              //break CheckLoop; //置けるなら周囲の確認終了
+              return [i, j];
+            }
+          } else {//CheckMyCell_valueが空セルの場合、その方向はチェックする必要ない
+            //alert("この方向には自分の石がありません")
+            break;
+          }
+        }
+      }
+      if (i == 7) {
+        //alert("このマスには置けません。");
+        return [-1, -1];
+      }
+    }
   } else { //選択したセルにすでに置かれている
-    alert("この場所にはすでに置かれています！　別の場所を選択してください");
+    //alert("この場所にはすでに置かれています！　別の場所を選択してください");
+    return [-1, -1];
   }
 }
 
+/*set stone*/
+function setstone(selected) {
+  var check_func = check_cell(selected);
+  var i = check_func[0];
+  var j = check_func[1];
+  var playernum = document.getElementById("player").innerText;
+
+  if (i < 0) {
+    alert("このマスには置けません！　別のマスを選択してください");
+  } else if (i > 0) {
+    for (var k = 0; k <= j; k++) {
+      /*選択されたセルに石を置く＆挟んだ相手の石の色を変える*/
+      document.getElementById(String(parseInt(selected.id) + direction[i] * k)).innerText = SymbolList[playernum - 1];
+    }
+    document.getElementById("player").innerText = playernum % 2 + 1;
+  }
+  count_stone();
+}
 
 
 /*置ける場所がない時はパス*/
-/*相手の石の色を変える*/
+for (x = 1; x < 9; x++) {
+  for (y = 1; y < 9; y++) {
+    var cell_id = String(x) + String(y);
+    
+  }
+}
+
